8681422b9c9fd09c1de78b49bea81ed2e74870a8
cwj <wjchoi@hancomfrontis.onmicrosoft.com>
2023-10-11 11:03:56 +0900

    체력 UI 추가

--- RealtimeFPS/Assets/Scripts/Network/Component/NetworkFPSPlayer.cs
+++ RealtimeFPS/Assets/Scripts/Network/Component/NetworkFPSPlayer.cs
@@ -10,11 +10,18 @@ public class NetworkFPSPlayer : NetworkComponent
         client.packetHandler.AddHandler(OnAttacked);
     }
 
-    private void OnAttacked( Protocol.S_ATTACKED pkt )
-    {
-        if (pkt.Playerid == objectId && pkt.Hp <= 0)
-        {
-            Destroy(gameObject);
-        }
-    }
+	private void OnAttacked(Protocol.S_ATTACKED pkt)
+	{
+		if (pkt.Playerid == objectId)
+		{
+			if (pkt.Hp <= 0) Debug.Log("죽었습니다.");
+
+			GameManager.UI.FetchPanel<Panel_HUD>().UpdateHealth(pkt.Hp);
+		}
+
+		else
+		{
+			Debug.Log(pkt.Playerid + "번 플레이어가 맞았습니다! 남은 HP : " + pkt.Hp);
+		}
+	}
 }
