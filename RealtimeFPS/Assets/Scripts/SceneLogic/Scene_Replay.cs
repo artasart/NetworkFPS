f8a35f34081cb7ef2be4ff974e01c8e586da461d
LHG <85724878+AmaneKanata@users.noreply.github.com>
2024-04-16 11:23:23 +0900

    리플레이 기능 구현중

--- RealtimeFPS/Assets/Scripts/SceneLogic/Scene_Replay.cs
+++ RealtimeFPS/Assets/Scripts/SceneLogic/Scene_Replay.cs
@@ -3,11 +3,17 @@ using Protocol;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
 
 public class Scene_Replay : MonoBehaviour
 {
+    private Dictionary<int, GameObject> players;
+
     void Start()
     {
+        players = new Dictionary<int, GameObject>();
+
         GameManager.Scene.Fade(false);
 
         Debug.Log("Scene_Replay Start");
@@ -18,7 +24,7 @@ public class Scene_Replay : MonoBehaviour
         NetworkManager.Instance.Client.Send(PacketManager.MakeSendBuffer(req));
     }
 
-    public void OnReplay( S_FPS_REPLAY pkt)
+    public void OnReplay( S_FPS_REPLAY pkt )
     {
         Debug.Log("OnReplay");
         Debug.Log(pkt.ReplayData.Count);
@@ -31,11 +37,130 @@ public class Scene_Replay : MonoBehaviour
     {
         for (int i = 0; i < pkt.ReplayData.Count; i++)
         {
-            Debug.Log(pkt.ReplayData[i]);
+            for (int j = 0; j < pkt.ReplayData[i].Instantiate.Count; j++)
+            {
+                HandleInstantiate(pkt.ReplayData[i].Instantiate[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].Position.Count; j++)
+            {
+                HandlePosition(pkt.ReplayData[i].Position[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].Rotation.Count; j++)
+            {
+                HandleRotation(pkt.ReplayData[i].Rotation[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].Shoot.Count; j++)
+            {
+                HandleShoot(pkt.ReplayData[i].Shoot[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].Attacked.Count; j++)
+            {
+                HandleAttacked(pkt.ReplayData[i].Attacked[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].Death.Count; j++)
+            {
+                HandleDeath(pkt.ReplayData[i].Death[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].ItemSpawn.Count; j++)
+            {
+                HandleItemSpawn(pkt.ReplayData[i].ItemSpawn[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].ItemDestroy.Count; j++)
+            {
+                HandleItemDestroy(pkt.ReplayData[i].ItemDestroy[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].DestinationSpawn.Count; j++)
+            {
+                HandleDestinationSpawn(pkt.ReplayData[i].DestinationSpawn[j]);
+            }
+
+            for (int j = 0; j < pkt.ReplayData[i].DestinationDestroy.Count; j++)
+            {
+                HandleDestinationDestroy(pkt.ReplayData[i].DestinationDestroy[j]);
+            }
+
             yield return new WaitForSeconds(0.05f);
         }
 
         GameManager.Scene.Fade(true);
         GameManager.Scene.LoadScene(SceneName.Lobby);
+    }
+
+    private void HandleInstantiate( Replay_Instantiate pkt )
+    {
+        Vector3 position = NetworkUtils.ConvertVector3(pkt.Position);
+        Quaternion rotation = NetworkUtils.ConvertQuaternion(pkt.Rotation);
+
+        GameObject player = UnityEngine.Object.Instantiate(Resources.Load<GameObject>("Prefab/Generic/PlayerCharacter_Remote"), position, rotation);
+
+        player.GetComponent<NetworkObject>().Client = NetworkManager.Instance.Client;
+        player.GetComponent<NetworkObject>().id = pkt.PlayerId;
+
+        player.name = pkt.PlayerId.ToString();
+
+        players.Add(pkt.PlayerId, player);
+    }
+
+    private void HandlePosition( Replay_Position pkt )
+    {
+        GameObject player;
+        players.TryGetValue(pkt.PlayerId, out player);
+        if(player == null)
+            return;
+
+        player.transform.position = NetworkUtils.ConvertVector3(pkt.Position);
+    }
+
+    private void HandleRotation( Replay_Rotation pkt )
+    {
+        GameObject player;
+        players.TryGetValue(pkt.PlayerId, out player);
+        if (player == null)
+            return;
+
+        player.transform.rotation = NetworkUtils.ConvertQuaternion(pkt.Rotation);
+    }
+
+    private void HandleShoot( Replay_Shoot pkt )
+    {
+
+    }
+
+    private void HandleAttacked( Replay_Attacked pkt )
+    {
+
+    }
+
+    private void HandleDeath( Replay_Death pkt )
+    {
+
+    }
+
+    private void HandleItemSpawn( Replay_ItemSpawn pkt )
+    {
+
+    }
+
+    private void HandleItemDestroy( Replay_ItemDestroy pkt )
+    {
+
+    }
+
+    private void HandleDestinationSpawn( Replay_DestinationSpawn pkt )
+    {
+
+    }
+
+    private void HandleDestinationDestroy( Replay_DestinationDestroy pkt )
+    {
+
     }
 }
